e0daa7a7a930af9d99195699687ea5b7c7526d1d
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-05-17 15:46:45 +0200

    funzione che crea la richiesta dei numeri

--- script.js
+++ script.js
@@ -12,4 +12,25 @@ while (numeriDaIndovinare.length < 5) {
 
 console.log(numeriDaIndovinare);
 // questo array vine esposto al utente 
+
+alert("memorizza questi 5 numeri, hai 10 secondi di tempo" + numeriDaIndovinare );
+// inizializzo la funzione della richiesta dei numeri con 10 secondi di ritardo
+setTimeout(requestNum, 10000);
 // da li l'utente ha 10 secondi per inserire i numeri corretti
+let userNum = [];
+
+
+function requestNum() {
+    for (let i = 0; i < 5; i++) {
+        let numRichiesto = parseInt(prompt("inserisci 5 numeri"));
+        if (!userNum.includes(numRichiesto) && numeriDaIndovinare.includes(numRichiesto)) {
+            userNum.push(numRichiesto)
+        }
+        
+    }
+    
+}
+
+console.log(userNum);
+
+// a seconda della scelta dell'utente crea una funzione che stampi quanti numeri ha indovinato
